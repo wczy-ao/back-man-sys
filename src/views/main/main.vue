502101649a939e539b8534835bb5d227d0c9d2e5
wan_z <1297285962qq.com>
2021-11-18 17:34:35 +0800

    refactor: 添加配置项

---
+++ src/views/main/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-button>ssss</el-button>
+  <el-button type="primary">提交</el-button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  // created() {},
+  // mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
